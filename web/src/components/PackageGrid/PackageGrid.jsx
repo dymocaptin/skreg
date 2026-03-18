3ee9a38f35932f5bd37c20fc0e8faf0f3b7e6abe
dymocaptin <>
2026-03-18 09:42:37 -0700

    feat(web): add VERIF header column to package grid

--- web/src/components/PackageGrid/PackageGrid.jsx
+++ web/src/components/PackageGrid/PackageGrid.jsx
@@ -150,6 +150,7 @@ export default function PackageGrid() {
                   <th className={styles.th}>NAME</th>
                   <th className={styles.th}>NAMESPACE</th>
                   <th className={styles.th}>VERSION</th>
+                  <th className={styles.th}>VERIFICATION</th>
                   {selectedIndex < 0 && <th className={styles.th}>DESCRIPTION</th>}
                   <th className={styles.th}></th>
                 </tr>
